77d5d2437b48deec3757edd92b4d41fe3c81d9a3
Victor Castro <soyvikman@gmail.com>
2020-09-10 00:42:08 -0500

    Agregadas rutas POST

--- routes/playas.js
+++ routes/playas.js
@@ -12,26 +12,36 @@ router.get("/", async(req,res)=>{
 
 // Agregar nueva playa
 // Ruta POST /playas/nueva
-/*
-router.post("/", async (req, res) => {
-    const { nombre, ruc, telefono, encargado, email } = req.body;
+
+router.post("/nueva", async (req, res) => {
+    const { nombre, latitud, longitud, aforo, bioseguridad } = req.body;
     try {
-        const nuevaEmpresa = new Empresa({
+        const nuevaPlaya = new Playa({
             nombre,
-            ruc,
-            telefono,
-            encargado,
-            email
+            latitud,
+            longitud,
+            aforo,
+            bioseguridad
         });
-        await nuevaEmpresa.save();
-        console.log(`Se agregó una nueva empresa: ${nuevaEmpresa}`);
-        res.json(nuevaEmpresa);
+        await nuevaPlaya.save();
+        console.log(`Se agregó una nueva playa: ${nuevaPlaya}`);
+        res.json(nuevaPlaya);
     } catch (error) {
         res.json(error);
     }
 });
-*/
 
+// Ver una playa
+// Ruta GET /playas/:id
+router.get("/:id", async (req, res)=>{
+    try{
+        const verPlaya = await Playa.findById(req.params.id);
+        res.json(verPlaya)
+    } catch (error){
+        console.log(`Se presentó el siguiente error: ${error}`)
+    }
+
+})
 
 
 module.exports = router;
